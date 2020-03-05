fcc49b216c17da4744e5d2bc4e9c0bd73e1bfaaa
celineTallage <61736623+celineTallage@users.noreply.github.com>
2020-03-05 11:03:53 +0100

    Create test.php

---
+++ test.php
@@ -0,0 +1,22 @@
+<?php
+
+$host = "80.247.226.20";
+$user = "root";
+$passM = "qX47etHy";
+$bdd = "strategiegrains";
+$conn = mysqli_connect( $host, $user, $passM, "$bdd")or die("Impossible de se connecter a la base de donnees");
+
+$qryDef = 'SELECT defaut FROM paramCampAffich where codeModule = "EXPC" and codeCult="BT"' ;
+$rsDef = $conn->query($qryDef);
+
+$nbDef  = $rsDef->num_rows ;
+
+
+$rsDef->data_seek(0);
+$row = $rsDef->fetch_row();
+$Campdef = $row[0];
+echo 'Campdef'.$Campdef;
+
+$rsDef->close();
+
+?>
