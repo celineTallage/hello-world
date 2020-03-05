d0555fadd6ce45e80735b9f001a1d7a8625e905d
celineTallage <61736623+celineTallage@users.noreply.github.com>
2020-03-05 11:19:44 +0100

    Update test.php

--- test.php
+++ test.php
@@ -2,8 +2,8 @@
 
 $host = "81.247.226.20";
 $user = "root";
-$passM = "qX47etHy";
-$bdd = "strategiegrains";
+$passM = "";
+$bdd = "";
 $conn = mysqli_connect( $host, $user, $passM, "$bdd")or die("Impossible de se connecter a la base de donnees");
 
 $qryDef = 'SELECT defaut FROM paramCampAffich where codeModule = "EXPC" and codeCult="BT"' ;
